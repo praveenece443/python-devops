9419ddef7b8ae6ed0f8fa19a9a5c672f5505f599
praveenece443 <praveenece443@gmail.com>
2023-05-13 15:36:31 +0530

    pytest cases

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("Loaded Successfully!!")
+from app import app
+
+def test1():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+
+def test2():
+    response = app.test_client().get('/dbbase')
+    assert response.status_code == 200
+
+def test3():
+    response = app.test_client().get('/')
+    assert b'Welcome to my Flask Login Page !!' in response.data
+
